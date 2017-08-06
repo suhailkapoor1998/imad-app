4c337ceb1714c3c9bfd5ad5acd437ec63bf95e37
suhailkapoor1998 <30747439+suhailkapoor1998@users.noreply.github.com>
2017-08-06 20:26:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articleOne={
+var articles={
+'article-one':{
     title:'Article One Suhail Kapoor',
     heading:'Article One',
     date:'Sep 5,2016',
@@ -24,6 +25,26 @@ var articleOne={
                     This is the content of my first article.
                 </p>`
     
+},
+'article-two':{
+     title:'Article Two Suhail Kapoor',
+    heading:'Article Two',
+    date:'Sep 10,2016',
+    content:`<p>
+                    This is the content of my second article.
+                    
+                </p>`
+    
+},
+'article-Three':{
+     title:'Article Three Suhail Kapoor',
+    heading:'Article Three',
+    date:'Sep 15,2016',
+    content:`<p>
+                    This is the content of my third article.
+                </p>`
+    
+}
 };
 function createTemplate(data){
     var title=data.title;
@@ -78,8 +99,9 @@ return htmlTemplate;
 
 
 `;}
-app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 app,get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
