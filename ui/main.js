805ec058c6b99cb940aced2dbba6d1470c7f21a7
suhailkapoor1998 <30747439+suhailkapoor1998@users.noreply.github.com>
2017-08-07 13:08:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ console.log('Loaded!');
 var elemnt=document.getElementById("main-text");
 element.innerHTML="New Value";
 //move the image
-var img = document.getElementById("img");
+var img = document.getElementById("madi");
 img.onclick=function() {
     img.style.marginleft='100px';
 }
