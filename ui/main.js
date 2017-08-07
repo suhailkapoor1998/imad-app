dbf02facd5a0a5767649018e56b506a5129fc851
suhailkapoor1998 <30747439+suhailkapoor1998@users.noreply.github.com>
2017-08-07 13:07:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+//change the tect of main-text div.
 var elemnt=document.getElementById("main-text");
 element.innerHTML="New Value";
+//move the image
+var img = document.getElementById("img");
+img.onclick=function() {
+    img.style.marginleft='100px';
+}
